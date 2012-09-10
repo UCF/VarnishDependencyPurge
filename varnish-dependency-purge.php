3af79952ae3e8ac131a3909e99af37b16807ca0c
Chris Conover <clcono@gmail.com>
2012-09-10 11:15:28 -0400

    Refactored out most static methods

--- varnish-dependency-purge.php
+++ varnish-dependency-purge.php
@@ -7,75 +7,27 @@ Author: Chris Conover
 License: GPL3
 */
 
-add_action('admin_init', create_function('', 'new VDP();'));
-add_action('admin_menu', create_function('', 'new VDPSettingsPage();'));
-register_activation_hook(__FILE__,   create_function('', 'VDP::activate();'));
-register_deactivation_hook(__FILE__, create_function('', 'VDP::deactivate();'));
-add_filter('query',    'vdp_query_filter');
-add_filter('shutdown', create_function('', 'VDP::write_posts();'));
-
-$vdp_post_ids = array();
-
-/**
- * Name this function so it can be removed and added again when VDP::write_posts
- * is called. As opposed to using create_function
- **/
-function vdp_query_filter($query) {
-	VDP::register_posts();
-	return $query;
-}
-
 class VDP {
-	public static
+	private static
 		$db_table_name = 'vdp_dependencies';
 
-	public static function get_db_table_name() {
-		global $wpdb;
-		return $wpdb->prefix.VDP::$db_table_name;
+	private
+		$vdp_post_ids  = array();
+
+	public function __construct() {
+		// Initialize the settings page
+		add_action('admin_menu', create_function('', 'new VDPSettingsPage();'));
+
+		// Register the activation and deactivation hooks
+		register_activation_hook(__FILE__,   array($this, 'activate'));
+		register_deactivation_hook(__FILE__, array($this, 'deactivate'));
+
+		// Record and write the dependent posts
+		add_filter('query',    array($this, 'query_filter'));
+		add_filter('shutdown', array($this, 'write_posts'));
 	}
 
-
-	/**
-	 * Construct the current page's URL. Varnish doesn't support SSL so don't
-	 * bother detecting it
-	 **/
-	public static function current_page_url() {
-		$page_url = 'http://'.$_SERVER['SERVER_NAME'];
-
-		if($_SERVER['SERVER_PORT'] != '80') {
-			$page_url .= ':'.$_SERVER['SERVER_PORT'];	
-		} 
-
-		$page_url .= $_SERVER['REQUEST_URI'];
-
-		return $page_url;
-	}
-
-	/**
-	 * Actions to execute when the plugin is activated
-	 **/
-	public static function activate() {
-		global $wpdb;
-
-		// Create dependency table
-		$definition = sprintf('CREATE TABLE %s (
-			page_url VARCHAR(2083) NOT NULL,
-			post_Id INT(11) NOT NULL,
-			KEY (post_id)
-		);', VDP::get_db_table_name());
-		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		dbDelta($definition);
-	}
-
-	/**
-	 * Actions to execute when the plugin is deactivated
-	 **/
-	public static function deactivate() {
-		global $wpdb;
-
-		// Drop the dependency table
-		$wpdb->query(sprintf('DROP TABLE IF EXISTS %s', VDP::get_db_table_name()));
-	}
+	/** Public Methods **/
 
 	/**
 	 * Parse the raw Varnish node option into VDPVarnishNode objects.
@@ -101,16 +53,53 @@ class VDP {
 	}
 
 	/**
+	 * Attached to the `query` action. Named function instead of anonymous
+	 * so that it can be unregistered before and re-registered after
+	 * writing the posts. Otherwise writing the posts would cause an infinite
+	 * loop because the `query` action would be triggered on each DB call. 
+	 **/
+	public function query_filter($query) {
+		$this->register_posts();
+		return $query;
+	}
+
+	/**
+	 * Actions to execute when the plugin is activated
+	 **/
+	public function activate() {
+		global $wpdb;
+
+		// Create dependency table
+		$definition = sprintf('CREATE TABLE %s (
+			page_url VARCHAR(2083) NOT NULL,
+			post_Id INT(11) NOT NULL,
+			KEY (post_id)
+		);', VDP::get_db_table_name());
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($definition);
+	}
+
+	/**
+	 * Actions to execute when the plugin is deactivated
+	 **/
+	public function deactivate() {
+		global $wpdb;
+
+		// Drop the dependency table
+		$wpdb->query(sprintf('DROP TABLE IF EXISTS %s', VDP::get_db_table_name()));
+	}
+
+	/**
 	 * Look at the last result from wpdb. If it contain's a post ID, assume
 	 * this URL relies on it and add it to the global vdp_post_ids list.
 	 **/
-	public static function register_posts() {
-		global $wpdb, $vdp_post_ids;
+	public function register_posts() {
+		global $wpdb;
 
 		if(!is_admin()) {
 			foreach($wpdb->last_result as $result) {
 				if(isset($result->post_id)) {
-					$vdp_post_ids[] = $result->post_id;
+					$this->vdp_post_ids[] = $result->post_id;
 				}
 			}
 		}
@@ -119,33 +108,32 @@ class VDP {
 	/**
 	 *	Write the posts collected by register_posts to the database.
 	 **/
-	public static function write_posts() {
-		global $wpdb, $vdp_post_ids;
-		
-		$current_page_url = VDP::current_page_url();
+	public function write_posts() {
+		global $wpdb;
 
-		// Don't retrigger vdp_query_filter when makes queries later in this
-		// function
-		remove_filter('query', 'vdp_query_filter');
+		$current_page_url = self::current_page_url();
+
+		// Don't retrigger register_posts when making queries later in this
+		// method
+		remove_filter('query', array($this, 'query_filter'));
 
 		// Call this once more to catch anything that happened between the last
 		// query filter and shutdown 
-		register_posts();
+		$this->register_posts();
 
 		// Delete the existing dependencies for this URL
-		$wpdb->query(sprintf(
-			'DELETE FROM %s WHERE page_url = %s',
-			VDP::get_db_table_name(),
+		$wpdb->query($wpdb->prepare(
+			'DELETE FROM '.self::get_db_table_name().' WHERE page_url = %s',
 			$current_page_url
 		));
 
 		// Only record each post id once
-		$vdp_post_ids = array_unique($vdp_post_ids);
+		$this->vdp_post_ids = array_unique($this->vdp_post_ids);
 
 		// Insert the new dependencies
-		foreach($vdp_post_ids as $post_id) {
+		foreach($this->vdp_post_ids as $post_id) {
 			$wpdb->insert(
-				VDP::get_db_table_name(),
+				self::get_db_table_name(),
 				array(
 					'page_url' => $current_page_url,
 					'post_id'  => $post_id
@@ -157,8 +145,33 @@ class VDP {
 			);
 		}
 
-		add_filter('query', 'vdp_query_filter');
+		add_filter('query', array($this, 'query_filter'));
 	}
+
+	/** Private Methods **/
+
+	private static function get_db_table_name() {
+		global $wpdb;
+		return $wpdb->prefix.self::$db_table_name;
+	}
+
+	/**
+	 * Construct the current page's URL. Varnish doesn't support SSL so don't
+	 * bother detecting it
+	 **/
+	private static function current_page_url() {
+		$page_url = 'http://'.$_SERVER['SERVER_NAME'];
+
+		if($_SERVER['SERVER_PORT'] != '80') {
+			$page_url .= ':'.$_SERVER['SERVER_PORT'];
+		} 
+
+		$page_url .= $_SERVER['REQUEST_URI'];
+
+		return $page_url;
+	}
+
+	
 }
 
 class VDPVarnishNode {
@@ -195,4 +208,6 @@ class VDPSettingsPage {
 		register_setting('vdp-settings-group', 'varnish-nodes');
 	}
 }
+
+$vdp = new VDP();
 ?>
