9f1475c40ec93c309b329c428ed738abf8fc7f58
Chris Conover <clcono@gmail.com>
2012-09-10 13:48:43 -0400

    Purge and Ban functionality

--- varnish-dependency-purge.php
+++ varnish-dependency-purge.php
@@ -9,12 +9,19 @@ License: GPL3
 
 class VDP {
 	private static
-		$db_table_name = 'vdp_dependencies';
+		$db_table_name = 'vdp_dependencies',
+		$purge_timeout = 3; // seconds
 
 	private
+		$varnish_nodes = array(),
 		$vdp_post_ids  = array();
 
 	public function __construct() {
+		// Parse the varnish nodes
+		if( ($node = self::parse_varnish_nodes()) !== False) {
+			$this->varnish_nodes = $nodes;
+		}
+
 		// Initialize the settings page
 		add_action('admin_menu', create_function('', 'new VDPSettingsPage();'));
 
@@ -25,6 +32,11 @@ class VDP {
 		// Record and write the dependent posts
 		add_filter('query',    array($this, 'query_filter'));
 		add_filter('shutdown', array($this, 'write_posts'));
+
+		// Purge URLs when a post is updated
+		add_action('publish_post', array($this, 'post_published'));
+		add_action('edit_post',    array($this, 'post_edited'));
+		add_action('deleted_post', array($this, 'post_deleted'));
 	}
 
 	/** Public Methods **/
@@ -43,7 +55,7 @@ class VDP {
 				$node_info_parts = explode(':', $node_info);
 				if(count($node_info_parts) == 2 && is_string($node_info_parts[0]) && 
 					$node_info_parts[0] != '' && is_numeric($node_info_parts[1])) {
-					$nodes[] = new VDPVarnishNode($node_info_parts[0], $node_info_parts[1]);
+					$nodes[] = new VDPVarnishNode($node_info_parts[0], $node_info_parts[1], self::$purge_timeout);
 				} else {
 					return False;
 				}
@@ -72,7 +84,7 @@ class VDP {
 		// Create dependency table
 		$definition = sprintf('CREATE TABLE %s (
 			page_url VARCHAR(2083) NOT NULL,
-			post_Id INT(11) NOT NULL,
+			post_id INT(11) NOT NULL,
 			KEY (post_id)
 		);', VDP::get_db_table_name());
 		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -115,7 +127,7 @@ class VDP {
 
 		// Don't retrigger register_posts when making queries later in this
 		// method
-		remove_filter('query', array($this, 'query_filter'));
+		$this->remove_query_filter();
 
 		// Call this once more to catch anything that happened between the last
 		// query filter and shutdown 
@@ -144,8 +156,72 @@ class VDP {
 				)
 			);
 		}
+		$this->add_query_filter();
+	}
 
-		add_filter('query', array($this, 'query_filter'));
+	private function post_modified($post_id) {
+		global $wpdb;
+
+		$this->remove_query_filter();
+
+		// Get all the URLs that depend on this post
+		$purge_urls = $wpdb->get_results($wpdb->prepare('
+			SELECT DISTINCT page_url FROM '.$this->get_db_table_name().' WHERE post_id = %d
+		', $post_id), ARRAY_A);
+
+		// Flatten the results
+		$purge_urls = array_map(create_function('$i', 'return $i[\'page_url\'];'), $page_urls);
+
+		// Always include this post's permalink
+		if( ($permalink = get_permalink($post_id)) !== False && !in_array($permalink, $purge_urls)) {
+			$purge_urls[] = $permalnk;
+		}
+
+		// Purge the URLs on each Varnish node
+		$success = True;
+		foreach($purge_urls as $purge_url) {
+			foreach($this->varnish_nodes as $node) {
+				if(!$node->purge($purge_url) && $success === True) {
+					$success = False;
+				}
+			}
+		}
+
+		$this->add_query_filter();
+
+		return $success;
+	}
+
+
+	/**
+	 * Since we can't know before hand the dependencies of this post, ban
+	 * the entire cache
+	 **/
+	public function post_published($post_id) {
+		foreach($this->varnish_nodes as $node) {
+			$node->ban('.');
+		}
+	}
+
+	/**
+	 * Purge associated URL. Deleted dependencies.
+	 **/
+	public function post_deleted($post_id) {
+		global $wpdb;
+
+		if($this->post_modified($post_id)) {
+			$this->remove_query_filter();
+			$wpdb->query($wpdb->prepare('DELETE FROM '.self::get_db_table_name().' post_id = %d', $post_id));
+			$this->add_query_filter();
+		}
+		
+	}
+
+	/**
+	 * Purse associated URLs
+	 **/
+	public function post_edited($post_id) {
+		$this->post_modified($post_id);
 	}
 
 	/** Private Methods **/
@@ -159,7 +235,7 @@ class VDP {
 	 * Construct the current page's URL. Varnish doesn't support SSL so don't
 	 * bother detecting it
 	 **/
-	private static function current_page_url() {
+	public static function current_page_url() {
 		$page_url = 'http://'.$_SERVER['SERVER_NAME'];
 
 		if($_SERVER['SERVER_PORT'] != '80') {
@@ -171,17 +247,76 @@ class VDP {
 		return $page_url;
 	}
 
+	private function remove_query_filter() {
+		remove_filter('query', array($this, 'query_filter'));
+	}
+	private function add_query_filter() {
+		add_filter('query', array($this, 'query_filter'));
+	}
 	
 }
 
 class VDPVarnishNode {
 	public
 		$host,
-		$port;
+		$port,
+		$timeout;
 
-	public function __construct($host, $port) {
-		$this->host = $host;
-		$this->port = $port;
+	public function __construct($host, $port, $timeout) {
+		$this->host    = $host;
+		$this->port    = $port;
+		$this->timeout = $timeout;
+	}
+
+	/**
+	 * Send a PURGE request to this Varnish node. Replace whatever
+	 * top level domain or ipaddress with the host and port of this node.
+	 **/
+	public function purge($url) {
+		
+		$request = setup_request($url, 'PURGE');
+		$success = make_request($request);
+		if(!$success) {
+			trigger_error('Varnish Depedency Purger: Unable to PURGE URL '.$url.'. The following error occurred: '.curl_error($request), E_USER_WARNING);
+			return False;
+		} else {
+			return True;
+		}
+	}
+
+	/**
+	 * Send a BAN request to this varnish node
+	 **/
+	public function ban($match) {
+		$request = setup_request(VDP::current_page_url(), 'BAN');
+		curl_setopt($request, CURLOPT_HTTPHEADER, array('X-Ban-Match: '.$match));
+		$success = make_request($request);
+		if(!$success) {
+			trigger_error('Varnish Depedency Purger: Unable to BAN match '.$match.'. The following error occurred: '.curl_error($request), E_USER_WARNING);
+			return False;
+		} else {
+			return True;
+		}
+	}
+
+	/**
+	 * Setup a CURL request to this Varnish node
+	 **/
+	private function setup_request($url, $method) {
+		$request = curl_init($url);
+		curl_setopt($request, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($request, CURLOPT_PORT,          $this->port);
+		curl_setopt($request, CURLOPT_TIMEOUT,       $this->timeout);
+		return $request;
+	}
+
+	/**
+	 * Complete the request created by setup_request
+	 **/
+	private function make_request($request) {
+		$success = curl_exec($request);
+		curl_close($request);
+		return $success;
 	}
 }
 
